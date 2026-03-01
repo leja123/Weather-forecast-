b0de51ecead4b65a862eddb545092e04ed970322
Leja Podobnik <leja123@users.noreply.github.com>
2026-03-01 18:40:54 +0100

    added weather icons

--- public/script.js
+++ public/script.js
@@ -6,10 +6,20 @@ async function loadWeather(city) {
         console.log(data);
 
         if (data.main) {
-            document.getElementById("cityName").innerHTML = document.getElementById("searchedCity").value;
+            let iconID = data.weather[0].icon;
+
+            document.getElementById("cityName").innerHTML = "Results for: " + document.getElementById("searchedCity").value;
             document.getElementById("weatherData").innerHTML =
                 "Temperature: " + data.main.temp +
                 "<br>Description: " + data.weather[0].description;
+
+            const iconContainer = document.getElementById('weatherIcon');
+            iconContainer.innerHTML = ""; 
+
+            const weatherImage = new Image(200, 200);
+            weatherImage.src = 'https://openweathermap.org/payload/api/media/file/' + iconID + '.png';
+            iconContainer.appendChild(weatherImage);    
+
         } else {
             document.getElementById("weatherData").innerHTML =
                 data.error;
@@ -25,7 +35,7 @@ function searchCity() {
     loadWeather(city);
 }
 
-loadWeather();
+
 
 
 
